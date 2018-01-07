db23a981aea03ebe013a42ceafd3c2baf255a0d7
Troy Kayne <troy@Troys-MacBook-Pro.local>
2018-01-07 16:29:18 -0700

    first commit for heroku

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+// REDUX CODE
+
+import 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+import ReactDom from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk';
+
+
+import reducers from './reducers';
+
+import App from './components/App';
+
+// create store
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.querySelector('#root'));
