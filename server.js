f2ef541c39e1aa4fabceb344986cfe6d282b655f
Troy Kayne <troy@Troys-MacBook-Pro.local>
2018-01-07 21:26:16 -0700

    Implemented auth flow

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
-var express = require('express');
+// load dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+//execute middleware
+require('./models/User');
+require('./services/passport');
+
+//connect to mongo
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({
-    title: 'API Server',
-    description: 'Powered by Express'
+// configure cookie life time
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    keys: [keys.cookieKey]
   })
-})
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/',
+  (req, res) => {
+    res.send({title:'My API route handler',description:'Powered By Express'})
+  }
+);
+// load route handlers
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.port || 5000;
 app.listen(PORT);
