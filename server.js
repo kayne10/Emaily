53406d86adb38103d26c1b541e91565df9310a17
Troy Kayne <troy@Troys-MacBook-Pro.local>
2018-01-07 17:27:50 -0700

    changed response method

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({
+  res.send({
     title: 'API Server',
     description: 'Powered by Express'
   })
